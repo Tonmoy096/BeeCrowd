2c0a52cdfa331b82c2b4b14f8bf53bec8db8040f
Tonmoy096 <foysalkabir2017@gmail.com>
2025-01-14 23:43:17 +0600

    refactor volume calculation program to streamline input and output

--- 1011.py
+++ 1011.py
@@ -1,8 +1,21 @@
 import math
 
-radius = float(input("Enter the radius of the sphere: "))
+radius = float(input())
 
 
 volume = (4.0 / 3) * math.pi * (radius ** 3)
+
+
+print(f"VOLUME = {volume:.3f}")
+
+
+pi = 3.14159
+
+
+radius = float(input())
+
+
+volume = (4.0 / 3) * pi * (radius ** 3)
+
 
 print(f"VOLUME = {volume:.3f}")
