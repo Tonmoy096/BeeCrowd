3a39fc4052025ea32b9afc348a9a44acb3f2b5c6
Tonmoy096 <foysalkabir2017@gmail.com>
2025-01-19 22:41:07 +0600

    add program to calculate fuel needed based on spent time and average speed

---
+++ 1017.py
@@ -0,0 +1,11 @@
+spent_time = int(input(" Enter the spent time (hours): "))
+
+average_speed = int(input(" Enter the evrage speed (km/h): "))
+
+distance = spent_time * average_speed
+
+per_km = 12
+
+fuel_needed = distance/per_km
+
+print(f"{fuel_needed:.3f}")
